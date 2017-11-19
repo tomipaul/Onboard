a1ce0d6af687ad5c57730633e77b6359f92e61c1
Tomi Paul <tomipaul95@gmail.com>
2017-11-19 23:01:29 +0100

    feat(server): set up application server - add package.json - add babel configuration - ignore appropriate folders/files - add server entry point
    [Delivers #152998495]

---
+++ src/server/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import winston from 'winston';
+
+dotenv.config();
+const app = express()
+const port = process.env.PORT || 5000;
+winston.configure({
+  transports: [
+    new (winston.transports.File)({ filename: 'server.log' })
+  ]
+});
+
+app.get('*', (req, res) => {
+  res.status(200).send('Welcome to the onboard API');
+});
+
+app.listen(port, () => {
+  winston.log('info', `Server started on ${port}`);
+});
+
+export default app;
