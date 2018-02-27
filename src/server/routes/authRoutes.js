dfc7f50ca9dee0a2c845fb289adb2216743d8e53
ayodelevm <ayodelevm@outlook.com>
2018-02-27 04:56:31 +0100

    feat(signin): feature implements the following: - refactored authenticateUser function to loginUser function - Added isAuthenticated function for token verification - Added and refactored function to validate payload - Refactored createUser method and route - Added sigin route and middleware

--- src/server/routes/authRoutes.js
+++ src/server/routes/authRoutes.js
@@ -2,9 +2,14 @@ import express from 'express';
 import AuthController from '../controllers/AuthController';
 
 const authRouter = express.Router();
-authRouter.post('/api/v1/user/signup', [
+authRouter.post(
+  '/api/v1/user/signup',
   AuthController.createUser(),
-  AuthController.authenticateUser()
-]);
+);
+
+authRouter.post(
+  '/api/v1/user/signin',
+  AuthController.loginUser()
+);
 
 export default authRouter;
