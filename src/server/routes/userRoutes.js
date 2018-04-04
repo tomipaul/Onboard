023f1b06d0919a07aeca843c155db7d74dc8e5b7
Tomi Paul <tomipaul95@gmail.com>
2018-04-04 03:21:14 +0100

    feat: get all saved sessions of a user
    [Finishes #153269610]

--- src/server/routes/userRoutes.js
+++ src/server/routes/userRoutes.js
@@ -3,11 +3,13 @@ import {
   changePassword,
   modifyProfile,
   resetPassword,
-  saveSession
+  saveSession,
+  getSessions,
 } from '../controllers/UserController';
 
 const userRouter = express.Router();
 
+userRouter.get('/api/v1/user/session', getSessions());
 userRouter.put('/api/v1/user', modifyProfile());
 userRouter.post('/api/v1/user/session', saveSession());
 userRouter.put('/api/v1/user/password', changePassword());
