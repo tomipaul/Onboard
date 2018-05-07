0c9ddfceabdad52d7560341d9c83e3cf34499740
ayodelevm <ayodelevm@outlook.com>
2018-05-07 15:49:55 +0100

    feature(delete session): Implements functionality to delete a session

--- src/server/routes/userRoutes.js
+++ src/server/routes/userRoutes.js
@@ -6,6 +6,7 @@ import {
   saveSession,
   getSessions,
   updateSession,
+  deleteSession,
 } from '../controllers/UserController';
 
 const userRouter = express.Router();
@@ -17,5 +18,6 @@ userRouter.patch('/api/v1/user/password/reset', resetPassword());
 userRouter.get('/api/v1/user/session', getSessions());
 userRouter.post('/api/v1/user/session', saveSession());
 userRouter.put('/api/v1/user/session/:sessionId', updateSession());
+userRouter.delete('/api/v1/user/session/:sessionId', deleteSession());
 
 export default userRouter;
